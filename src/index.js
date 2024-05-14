7d2a96e7fea06ac939957570d7e688e2a48fd298
Mayra Landa <mlandae16@gmail.com>
2024-05-14 16:52:57 -0700

    add function that extract data

--- src/index.js
+++ src/index.js
@@ -1,33 +1,44 @@
-async function getLocationData(location){
+async function getLocationData(location) {
   const apiKey = '850c0f76ad8944c6806162632240605';
   const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${encodeURIComponent(
-  location)}`;
+    location
+  )}`;
 
-  try{
+  try {
     const response = await fetch(apiUrl);
-    if (!response.ok){
-      throw new Error("Failed to fetch data");
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
     }
     const data = await response.json();
-    return data;
-  } catch (error){
-    console.error("Error fetching data:", error);
+
+    const extractedData = {
+      location: data.location.name,
+      region: data.location.region,
+      currentTemperature: data.current.feelslike_f,
+      condition: data.current.condition.text,
+      humidity: data.current.humidity,
+      temperature: data.current.temp_f,
+      wind: data.current.wind_mph,
+    };
+    return extractedData;
+  } catch (error) {
+    console.error('Error fetching data:', error);
     return null;
   }
 }
 
-async function fetchAndLogData(location){
+async function fetchAndLogData(location) {
   try {
     const data = await getLocationData(location);
-    if (data){
+    if (data) {
       console.log(data);
     } else {
-      console.log("No data available");
-    } 
-  } catch (error){
-    console.error("Error:", error);
+      console.log('No data available');
+    }
+  } catch (error) {
+    console.error('Error:', error);
   }
 }
 
-const location = prompt("Please enter a location");
+const location = prompt('Please enter a location');
 fetchAndLogData(location);
